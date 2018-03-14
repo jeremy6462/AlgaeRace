682c64816d0c3b20c35da7939fbfaaec10613d62
jeremy6462 <jkjak6@gmail.com>
2018-03-14 19:50:35 -0400

    movement using timers
    This is a commit to use timers to trigger movement. This would be good to use for long pressing on sides of the screen for movement. But I think I should switch to vigorous tapping for movement

--- AlgaeRace.playground/Contents.swift
+++ AlgaeRace.playground/Contents.swift
@@ -14,18 +14,58 @@ struct Fish {
 
 }
 
-struct FishMover {
+class FishMover {
     
-    let fish: Fish
+    enum Direction {
+        case right
+        case left
+    }
+    
+    private var fish: Fish
+    private var currentMovementTimer: Timer?
+    
+    weak var delegate: FishMovementSubscriber?
     
     init(fish: Fish) {
         self.fish = fish
     }
     
-    func move(by distance: ScreenPercentage, every second: TimeInterval) {
+    func move(by distance: ScreenPercentage, every timeAmount: TimeInterval, towards direction: Direction) {
         
+        if let existingTimer = self.currentMovementTimer {
+            existingTimer.invalidate()
+            self.currentMovementTimer = nil
+        }
         
+        var movement: ScreenPercentage
+        
+        switch direction {
+        case .right:
+            movement = distance
+        case .left:
+            movement = -distance
+        }
+        
+        let movementTimer = Timer(timeInterval: timeAmount, repeats: true) { (timer) in
+            self.fish.currentPosition += distance
+            self.delegate?.movement(of: self.fish, by: distance, towards: direction)
+        }
+        
+        self.currentMovementTimer = movementTimer
+        
+        movementTimer.fire()
         
     }
     
+    func stopCurrentMovement() {
+        self.currentMovementTimer?.invalidate()
+        self.currentMovementTimer = nil
+    }
+    
 }
+
+protocol FishMovementSubscriber: class {
+    func movement(of fish: Fish, by distance: ScreenPercentage, towards direction: FishMover.Direction)
+}
+
+
