836fe5a30a3b44ebe0738e60b98f3b6a7c9c0892
Jeremy Kelleher <jkjak6@gmail.com>
2018-04-07 17:50:38 -0400

    Localize movement and oxygen code in fish object

--- AlgaeRace.playground/Contents.swift
+++ AlgaeRace.playground/Contents.swift
@@ -2,70 +2,69 @@
 
 import UIKit
 
-typealias ScreenPercentage = Double
+/*
+ Game: Algae Race
+ Premise: User controls a fish that stays at the bottom of the screen and can move left and right by tapping the right and left sides of the screen. They're goal is to stay alive by collecting oxygen bubbles that gradually and navigate around the ever growing population of algae. The algae should begin competing with the fish (albeit slower, then the fish) to get the oxygen. As the game continues, oxygen become more spares because the algae is overpopulated. The scene gets darker and there's less and less oxygen. Eventually, the fish dies and the hope is that the students compare times with each other for how long they stayed alive.
+ Learning Goals:
+    - Sun light causes algae to grow
+    - Oxygen is necessary for live for the fish
+    - Once algae overcome carrying capacity, they begin consuming oxygen and competing with the fish
+ 
+ Oxygen Changes:
+    - Goes up: fish touches an oxygen bubble
+    - Goes down: small rate of decrease over time, larger rate for each movement
+ 
+ Constant increasing rate of algae growth, but when oxygen becomes very sparse, the rate decreases to 0
+ */
+
+typealias DecimalPercentage = Double
 
 struct Fish {
     
+    // MARK: Properties
+    
     /// the amount of oxygen the fish currently has (represented as a percentage)
-    var oxygenSupply = 1.0
+    private(set) var oxygenSupply: DecimalPercentage
     
     /// the current position of the fish onscreen (represented as a percentage of the horizontal screen points
-    var currentPosition: ScreenPercentage = 0.5
-
+    var currentHorizontalPosition: DecimalPercentage
+    
+    // MARK: Subscribers
+    weak var oxygenSubscriber: FishOxygenSupplySubscriber?
+    weak var movementSubscriber: FishMovementSubscriber?
+    
+    // MARK: Rates
+    private let OXYGEN_MOVEMENT_COST: DecimalPercentage = 0.02
+    
+    var isAlive: Bool {
+        return oxygenSupply != 0.0
+    }
+    
+    init(oxygenSupply: DecimalPercentage = 1.0, horizontalPosition: DecimalPercentage = 0.5) {
+        self.oxygenSupply = oxygenSupply
+        self.currentHorizontalPosition = horizontalPosition
+    }
+    
+    mutating func move(by amount: DecimalPercentage) {
+        self.currentHorizontalPosition += amount
+        self.movementSubscriber?.movement(of: self, to: self.currentHorizontalPosition)
+        self.updateOxygenSupply(by: OXYGEN_MOVEMENT_COST)
+        
+    }
+    
+    mutating func updateOxygenSupply(by amount: DecimalPercentage) {
+        self.oxygenSupply += amount
+        self.oxygenSubscriber?.oxygenSupply(of: self, didUpdateTo: self.oxygenSupply)
+    }
+    
 }
 
-class FishMover {
-    
-    enum Direction {
-        case right
-        case left
-    }
-    
-    private var fish: Fish
-    private var currentMovementTimer: Timer?
-    
-    weak var delegate: FishMovementSubscriber?
-    
-    init(fish: Fish) {
-        self.fish = fish
-    }
-    
-    func move(by distance: ScreenPercentage, every timeAmount: TimeInterval, towards direction: Direction) {
-        
-        if let existingTimer = self.currentMovementTimer {
-            existingTimer.invalidate()
-            self.currentMovementTimer = nil
-        }
-        
-        var movement: ScreenPercentage
-        
-        switch direction {
-        case .right:
-            movement = distance
-        case .left:
-            movement = -distance
-        }
-        
-        let movementTimer = Timer(timeInterval: timeAmount, repeats: true) { (timer) in
-            self.fish.currentPosition += distance
-            self.delegate?.movement(of: self.fish, by: distance, towards: direction)
-        }
-        
-        self.currentMovementTimer = movementTimer
-        
-        movementTimer.fire()
-        
-    }
-    
-    func stopCurrentMovement() {
-        self.currentMovementTimer?.invalidate()
-        self.currentMovementTimer = nil
-    }
-    
+protocol FishOxygenSupplySubscriber: class {
+    func oxygenSupply(of fish: Fish, didUpdateTo oxygenSupply: DecimalPercentage)
 }
 
 protocol FishMovementSubscriber: class {
-    func movement(of fish: Fish, by distance: ScreenPercentage, towards direction: FishMover.Direction)
+    func movement(of fish: Fish, to position: DecimalPercentage)
 }
 
 
